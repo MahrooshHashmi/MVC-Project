9def2fde4f89bd8fdf3fe5c0894b7151231a4fcc
Mudassar <mudassarhassan135@gmail.com>
2019-03-17 09:22:08 +0500

    MVC Bug fix

--- Ead-MVC - Copy/Ead-MVC/Controllers/HomeController.cs
+++ Ead-MVC - Copy/Ead-MVC/Controllers/HomeController.cs
@@ -15,15 +15,16 @@ namespace Ead_MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+         
+            return View();
+        }
+        public ActionResult Login()
+        {
             return View();
         }
     }
